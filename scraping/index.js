20b0a230bd2ae2826111733139f2c823410f834b
abhishekkakolla <70043537+abhishekkakolla@users.noreply.github.com>
2024-01-12 15:51:00 -0800

    scraping

--- scraping/index.js
+++ scraping/index.js
@@ -4,11 +4,26 @@ async function run() {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
-    await page.goto('https://www.traversymedia.com');
+    await page.goto('https://www.youtube.com/results?search_query=k+suave');
 
 
 
-    // await page.pdf({ path: 'example.pdf', format: 'A4' });
+    // await page.screenshot({ path: 'example.png'});
+    const videos = await page.evaluate(() => Array.from(document.querySelectorAll('#contents #dismissible'), (e) => ({
+        title: e.querySelector('#video-title').innerText, 
+        viewcount: e.querySelector('#metadata-line span').innerText,
+    })));
+
+    
+
+
+
+
+
+    console.log(videos);
+
+    const li = document.get
+    document.getElementById("content").appendChild(document.createTextNode())
 
 
     const html = await page.content();
